8854e423fcebbc5f3427a928a23a048818dcf74f
zapzookj <154570825+zapzookj@users.noreply.github.com>
2024-01-16 19:52:28 +0900

    [level Lv.1] Title: 문자열 내 마음대로 정렬하기, Time: 1.20 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (a, b) -> {
+            char charA = a.charAt(n);
+            char charB = b.charAt(n);
+            
+            if (charA == charB) {
+                return a.compareTo(b);
+            }
+            
+            return charA - charB;
+        });
+        
+        return strings;
+    }
+}
+// 문제의 제한 조건을 제외하고 목적만을 본다면, 결국 주어지는 문자열을 '정렬'하는 것이다.
+// Arrays.sort(a, b) 메서드를 통해, 주어진 제한 조건을 자세히 정의하는 b를 람다 식으로 정의한다면,
+// 효율적으로 문제를 해결할 수 있다. 해당 코드에서 제한 조건 b는 (a, b) -> {람다식} 이 되겠다.

1b2b3096ffafc587301b467d1a42f073fecba57f
zapzookj <154570825+zapzookj@users.noreply.github.com>
2024-01-19 09:50:50 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        int num1 = 0;
+        int num2 = 0;
+        int temp1 = 0;
+        int temp2 = 0;
+        for(int i = 0; i < goal.length; i++){
+            if(goal[i].equals(cards1[num1])){
+                if(num1<cards1.length-1){num1++;}else{temp1++;}
+            }else if(goal[i].equals(cards2[num2])){
+                if(num2<cards2.length-1){num2++;}else{temp2++;}
+            }
+        }
+        // (goal.length - 1) = num1+num2 ? answer="Yes" : answer="No";
+        answer = (goal.length)==num1+num2+temp1+temp2 ? "Yes":"No";
+        return answer;
+    }
+}

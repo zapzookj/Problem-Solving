63a52b57cb90fe12aff42cf12eafde4a469315f4
zapzookj <154570825+zapzookj@users.noreply.github.com>
2024-02-05 10:15:53 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.46 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        
+        int zeroCount = 0; // 0의 개수
+        int matchCount = 0; // 일치하는 번호의 개수
+        
+        for (int lotto : lottos) {
+            if (lotto == 0) {
+                zeroCount++;
+                continue;
+            }
+            for (int win_num : win_nums) {
+                if (lotto == win_num) {
+                    matchCount++;
+                    break;
+                }
+            }
+        }
+
+        int maxRank = getRank(matchCount + zeroCount); // 최대 순위 (가능한 모든 0이 당첨번호와 일치한다고 가정)
+        int minRank = getRank(matchCount); // 최소 순위 (0이 당첨번호와 일치하지 않는다고 가정)
+
+        return new int[]{maxRank, minRank};
+    }
+
+    private int getRank(int matchCount) {
+        switch (matchCount) {
+            case 6: return 1;
+            case 5: return 2;
+            case 4: return 3;
+            case 3: return 4;
+            case 2: return 5;
+            default: return 6; // 1개 이하로 일치하거나 하나도 일치하지 않는 경우
+        }
+    }
+}

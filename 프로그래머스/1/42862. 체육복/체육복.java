1f9e755e893d5b89add49cdc72d1c2aa08c7dc8f
zapzookj <154570825+zapzookj@users.noreply.github.com>
2024-03-05 09:11:27 +0900

    [level 1] Title: 체육복, Time: 0.50 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -6,24 +6,21 @@ class Solution {
         List<Integer> lostList = new ArrayList<>();
         List<Integer> reserveList = new ArrayList<>();
 
-        // 초기화
         for (int num : lost) lostList.add(num);
         for (int num : reserve) reserveList.add(num);
         
         Collections.sort(lostList);
         Collections.sort(reserveList);
-
-        // 여벌의 체육복을 가져온 학생이 도난당한 경우
+        
         lostList.removeIf(lostNum -> reserveList.remove(Integer.valueOf(lostNum)));
 
-        // 도난당한 학생에게 체육복 빌려주기
         int cut = 0;
         for (Integer lostNum : new ArrayList<>(lostList)) {
             for (int j = 0; j < reserveList.size(); j++) {
                 if (Math.abs(reserveList.get(j) - lostNum) == 1) {
                     cut++;
                     reserveList.remove(j);
-                    break; // 해당 학생에게 체육복을 빌려준 후 루프 종료
+                    break;
                 }
             }
         }

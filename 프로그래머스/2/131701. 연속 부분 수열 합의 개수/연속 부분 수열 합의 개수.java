9b17b86b7a36742eca0d42b13b32b7dc3875cb59
zapzookj <154570825+zapzookj@users.noreply.github.com>
2024-07-17 23:16:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 130.90 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,30 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int n = elements.length;
-        int[] extendedElements = new int[n * 2];
-        
-        // 원형 수열을 표현하기 위해 배열을 두 번 반복
-        for (int i = 0; i < n; i++) {
-            extendedElements[i] = extendedElements[i + n] = elements[i];
-        }
-        
-        // 누적 합 계산
-        int[] prefixSum = new int[n * 2 + 1];
-        for (int i = 1; i <= n * 2; i++) {
-            prefixSum[i] = prefixSum[i - 1] + extendedElements[i - 1];
-        }
-        
-        Set<Integer> sums = new HashSet<>();
-        
-        // 모든 가능한 부분 수열의 합 계산
-        for (int len = 1; len <= n; len++) {
-            for (int start = 0; start < n; start++) {
-                int sum = prefixSum[start + len] - prefixSum[start];
-                sums.add(sum);
+            Set<Integer> set = new HashSet<>();
+            int[] dp = new int[elements.length];
+            for(int len = 1;len <= elements.length; len++){
+                for(int i = 0;i<elements.length;i++){
+                    dp[i] += elements[(len+i-1)%elements.length];
+                    set.add(dp[i]);
+                }
             }
+            return set.size();
         }
-        
-        return sums.size();
-    }
 }

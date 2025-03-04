338fa4329afa1f69bbb6005f6cf68ede2e5a6265
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-04 10:41:09 +0900

    [level 1] Title: 시저 암호, Time: 2.05 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append((char) ('A' + (c - 'A' + n) % 26));
+            } else if (Character.isLowerCase(c)) {
+                sb.append((char) ('a' + (c - 'a' + n) % 26));
+            } else {
+                sb.append(' ');
+            }
+        }
+        
+        return sb.toString();
+    }
+}

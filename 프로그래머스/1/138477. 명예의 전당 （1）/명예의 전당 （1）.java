7447b44923443a0cf5ebcd499f6164e1558859fd
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-07 10:36:23 +0900

    [level 1] Title: 명예의 전당 (1), Time: 19.14 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,20 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 class Solution {
     public List solution(int k, int[] score) {
-        List<Integer> answer = new ArrayList<>();
         List<Integer> jundang = new ArrayList<>();
-        for(int i = 0; i<score.length; i++){
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++) {
             jundang.add(score[i]);
-            Collections.sort(jundang);
-            Collections.reverse(jundang);
-            if(i<k){
-                answer.add(jundang.get(i));
-            }else{
-                answer.add(jundang.get(k-1));
+            jundang.sort(Comparator.reverseOrder());
+            if (i < k) {
+                result.add(jundang.get(i));
+            } else {
+                result.add(jundang.get(k-1));
             }
         }
-        return answer;
+        
+        return result;
     }
 }
+
+//Collections.reverse() 메서드를 통해 오름차순으로 나열된 리스트를 뒤집어 내림차순으로 정렬할 수 있다.

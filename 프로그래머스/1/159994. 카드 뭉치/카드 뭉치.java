0fceae4c42f96c7cef9d2601db9c986629813651
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-09 21:24:49 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -5,15 +5,14 @@ class Solution {
         int num2 = 0;
         int temp1 = 0;
         int temp2 = 0;
-        for(int i = 0; i < goal.length; i++){
-            if(goal[i].equals(cards1[num1])){
-                if(num1<cards1.length-1){num1++;}else{temp1++;}
-            }else if(goal[i].equals(cards2[num2])){
-                if(num2<cards2.length-1){num2++;}else{temp2++;}
+        for (int i = 0; i < goal.length; i++) {
+            if (goal[i].equals(cards1[num1])) {
+                if (num1<cards1.length-1) {num1++;} else {temp1++;}
+            } else if (goal[i].equals(cards2[num2])) {
+                if (num2<cards2.length-1) {num2++;} else {temp2++;}
             }
         }
-        // (goal.length - 1) = num1+num2 ? answer="Yes" : answer="No";
-        answer = (goal.length)==num1+num2+temp1+temp2 ? "Yes":"No";
+        answer = (goal.length) == num1+num2+temp1+temp2 ? "Yes":"No";
         return answer;
     }
 }

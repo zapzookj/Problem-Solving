6df1d9da79efba941d08dfb7f636a87a37cbc03f
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-11 17:33:57 +0900

    [level 1] Title: 모의고사, Time: 1.74 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,7 +7,9 @@ class Solution {
             {2,1,2,3,2,4,2,5},
             {3,3,1,1,2,2,4,4,5,5}
         };
+        
         int[] scores = new int[3];
+        
         for(int i = 0; i < answers.length; i++){
             for(int j = 0; j < 3; j++){
                 if(answers[i] == patterns[j][i % patterns[j].length]){
@@ -15,9 +17,11 @@ class Solution {
                 }
             }
         }
+        
         int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
         List<Integer> result = new ArrayList<>();
+        
         for (int i = 0; i < 3; i++) {
             if (scores[i] == maxScore) {
                 result.add(i + 1);

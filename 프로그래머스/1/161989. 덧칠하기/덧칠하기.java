4158b9dcace643f74370d6c534f6be27c87d68d1
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-12 10:43:51 +0900

    [level 1] Title: 덧칠하기, Time: 2.16 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int result = 0;
+        int result = 1;
+        int roller = section[0];
         
-        while (section.length > 0) {
-            int cover = section[0] + m - 1;
-            section = Arrays.stream(section)
-                .filter(num -> num > cover)
-                .toArray();
-            result++;
+        for (int i = 0; i < section.length; i++) {
+            if (roller + m - 1 < section[i]) {
+                result++;
+                roller = section[i];
+            }
         }
         
         return result;

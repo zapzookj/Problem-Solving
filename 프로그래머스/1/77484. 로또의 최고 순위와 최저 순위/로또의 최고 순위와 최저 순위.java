cebc595a84bb7145a701cf37c7f74589685ebe4d
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-13 10:24:15 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,42 +1,73 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        Arrays.sort(lottos);
-        Arrays.sort(win_nums);
+        int[] result = new int[2];
+        int max = 0;
+        int min = 0;
         
-        int zeroCount = 0; 
-        int matchCount = 0; 
+        Set<Integer> set = new HashSet<>();
+        for (int num : win_nums) {
+            set.add(num);
+        }
         
-        for (int lotto : lottos) {
-            if (lotto == 0) {
-                zeroCount++;
-                continue;
-            }
-            for (int win_num : win_nums) {
-                if (lotto == win_num) {
-                    matchCount++;
-                    break;
-                }
+        for(int lot : lottos) {
+            if (set.contains(lot)) {
+                max++;
+                min++;
+            } else if (lot == 0) {
+                max++;
             }
         }
-
-        int maxRank = getRank(matchCount + zeroCount); // 최대 순위 (가능한 모든 0이 당첨번호와 일치한다고 가정)
-        int minRank = getRank(matchCount); // 최소 순위 (0이 당첨번호와 일치하지 않는다고 가정)
-
-        return new int[]{maxRank, minRank};
-    }
-
-    private int getRank(int matchCount) {
-        switch (matchCount) {
-            case 6: return 1;
-            case 5: return 2;
-            case 4: return 3;
-            case 3: return 4;
-            case 2: return 5;
-            default: return 6; // 1개 이하로 일치하거나 하나도 일치하지 않는 경우
+        
+        switch (max) {
+            case 0:
+                result[0] = 6;
+                break;
+            case 1:
+                result[0] = 6;
+                break;
+            case 2:
+                result[0] = 5;
+                break;
+            case 3:
+                result[0] = 4;
+                break;
+            case 4:
+                result[0] = 3;
+                break;
+            case 5:
+                result[0] = 2;
+                break;
+            case 6:
+                result[0] = 1;
+                break;
         }
+        
+        switch (min) {
+            case 0:
+                result[1] = 6;
+                break;
+            case 1:
+                result[1] = 6;
+                break;
+            case 2:
+                result[1] = 5;
+                break;
+            case 3:
+                result[1] = 4;
+                break;
+            case 4:
+                result[1] = 3;
+                break;
+            case 5:
+                result[1] = 2;
+                break;
+            case 6:
+                result[1] = 1;
+                break;
+        }
+        
+        return result;
     }
 }

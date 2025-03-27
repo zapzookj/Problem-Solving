649567c65b578bdf6b8d2bd1f31ca3003a552226
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-27 20:19:41 +0900

    [level 1] Title: 햄버거 만들기, Time: 69.49 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -10,14 +10,13 @@ class Solution {
             if (list.size() >= 4 && list.get(list.size() - 4) == 1 &&
                 list.get(list.size() - 3) == 2 && list.get(list.size() - 2) == 3 &&
                 list.get(list.size() - 1) == 1) {
-                // 패턴이 발견되었을 때, 해당 패턴이 시작된 인덱스부터 4개의 요소를 제거
                 for (int j = 0; j < 4; j++) {
                     list.remove(list.size() - 1);
                 }
-                // 패턴을 찾았으므로 answer를 1 증가시킴
                 answer++;
             }
         }
+        
         return answer;
     }
 }

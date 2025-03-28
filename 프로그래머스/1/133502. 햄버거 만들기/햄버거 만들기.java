b452c69aba5cb9fd8355337ab03cfe67b87155ff
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-28 16:26:45 +0900

    [level 1] Title: 햄버거 만들기, Time: 22.17 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,21 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < ingredient.length; i++) {
-            list.add(ingredient[i]);
-            if (list.size() >= 4 && list.get(list.size() - 4) == 1 &&
-                list.get(list.size() - 3) == 2 && list.get(list.size() - 2) == 3 &&
-                list.get(list.size() - 1) == 1) {
-                for (int j = 0; j < 4; j++) {
-                    list.remove(list.size() - 1);
-                }
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
                 answer++;
             }
         }
-        
         return answer;
     }
 }

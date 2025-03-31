dbd9d6efd3c788d52ba14e38172d25a560ac6ba4
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-03-31 10:51:42 +0900

    [level 1] Title: 달리기 경주, Time: 552.20 ms, Memory: 355 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 371 MB, 시간: 392.40 ms
+메모리: 355 MB, 시간: 552.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 16일 01:57:27
+2025년 03월 31일 10:51:39
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -8,11 +8,11 @@ class Solution {
         Map<String, Integer> rankMap = new HashMap<>();
         List<String> playerList = new ArrayList<>();
         
-        for(int i = 1; i <= players.length; i++) {
+        for (int i = 1; i <= players.length; i++) {
             rankMap.put(players[i-1], i);
             playerList.add(players[i-1]);
         }
-        for(int i = 0; i < callings.length; i++) {
+        for (int i = 0; i < callings.length; i++) {
             Integer tempRank = rankMap.get(callings[i]); // 호명된 선수의 원래 등수
             String tempPlayer = playerList.get(tempRank - 2); // 호명된 선수에게 추월당한 선수
             playerList.set(tempRank - 2, callings[i]); // 호명된 선수를 추월당한 선수의 위치로 옮김
@@ -20,7 +20,7 @@ class Solution {
             rankMap.put(callings[i], tempRank-1); // rankMap도 변경
             rankMap.put(tempPlayer, tempRank);
         }
-        for(int i = 0; i < answer.length; i++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = playerList.get(i);
         }
       

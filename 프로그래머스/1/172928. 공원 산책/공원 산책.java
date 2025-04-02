24f7f4d287c3a30b75ab4137a4a1f03a08033b6f
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-04-02 10:55:08 +0900

    [level 1] Title: 공원 산책, Time: 0.56 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -13,7 +13,6 @@ class Solution {
             String[] parts = route.split(" ");
             String direction = parts[0];
             int distance = Integer.parseInt(parts[1]);
-            
             // 이동 후의 잠정적인 새로운 위치 계산
             int newY = currentY;
             int newX = currentX;
@@ -29,14 +28,12 @@ class Solution {
                 } else if (direction.equals("E")) {
                     newX++;
                 }
-                
                 // 공원을 벗어나거나 장애물을 만나면 무효 처리
                 if (newY < 0 || newY >= H || newX < 0 || newX >= W || park[newY].charAt(newX) == 'X') {
                     isValid = false;
                     break;
                 }
             }
-            
             // 유효한 이동이면 위치 업데이트
             if (isValid) {
                 currentY = newY;

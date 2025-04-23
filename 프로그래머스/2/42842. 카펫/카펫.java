3adec8e83fc0786da46f1197ec6a37a868392d3d
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-04-23 23:23:37 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int block = brown + yellow;
+        int row = 0;
+        int col = 0;
+        
+        for (int i = 3; i <= Math.sqrt(block); i++) {
+            if(block % i == 0) {
+                row = i;
+                col = block / i;
+                if((row - 2) * (col - 2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+                return answer;
+            }
+            }
+            
+        }
+        
+        return answer;
+    }
+}

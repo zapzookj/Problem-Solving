8968e63ecb14fd2dc06d6185f33776189107f510
zapzookj <154570825+zapzookj@users.noreply.github.com>
2025-05-21 21:38:10 +0900

    [level 2] Title: 피로도, Time: 8.62 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -4,17 +4,17 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        explore(k, dungeons, 0);
+        dfs(k, dungeons, 0);
         return max;
     }
     
-    private static void explore(int k, int[][] dungeons, int count) {
+    private static void dfs(int k, int[][] dungeons, int count) {
         max = Math.max(max, count);
         
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                explore(k - dungeons[i][1], dungeons, count + 1);
+                dfs(k - dungeons[i][1], dungeons, count + 1);
                 visited[i] = false;
             }
         }
